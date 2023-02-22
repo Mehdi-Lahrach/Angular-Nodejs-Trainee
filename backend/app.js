e6e1093bd307ff4c3ee45fa2bae5d3f876d6f9fe
unknown <mehdilahrach89@gmail.com>
2023-02-22 17:01:34 +0100

    getting started with mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose'); //importing mongoose
+const Post = require('./models/post'); //importing the post model
+
 const app = express();
+
+//Jns6#$-ttBS5Ubw password for mongodb account
+mongoose.connect("mongodb+srv://mehdi:IbXWQftVK2n3iMRU@cluster0.ucprsgw.mongodb.net/node-angular?retryWrites=true&w=majority").
+//connecting to the database
+then(() => {console.log('Connected to database!')})
+.catch(() => {console.log('Connection failed!')});
 
 app.use(bodyParser.json()); //middleware to parse the body of the request
 app.use(bodyParser.urlencoded({extended: false})); //middleware to parse the body of the request
@@ -16,8 +26,14 @@ app.use((req,res,next)=>{ //middleware to allow cross origin requests
 
 
 app.post('/api/posts',(req,res,next)=>{ //middleware to handle post requests
-  const post = req.body; //extracting the post from the request body
+  const post = new Post({ //creating a new post
+    title: req.body.title, //getting the title from the request body
+    content: req.body.content //getting the content from the request body
+  });
+
   console.log(post); //logging the post to the console
+  post.save(); //saving the post to the database
+
   res.status(201).json({ //sending the response as json
     message: 'Post added successfully' //sending a message to the client
   });
